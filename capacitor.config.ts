842b73269a1b05f214a44c45ede8a143209816f5
JoeTest Hendricks <joehendricks415@gmail.com>
2023-08-12 13:19:43 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'moodFoods',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
